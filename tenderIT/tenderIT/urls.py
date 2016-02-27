e6b24fae94e061d4e852043345d2a2d9481e3f34
ismayilrza <irgbo@yahoo.com>
2016-02-27 22:58:41 +0000

    urls modfied

--- tenderIT/tenderIT/urls.py
+++ tenderIT/tenderIT/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tenderIT.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^tenderIT/', include('TenderITApp.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
 
-)
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^TenderITApp/', include('TenderITApp.urls')),
+               ]
