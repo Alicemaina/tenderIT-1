de4f4bf708b96bede91b92a4cce5ff5cb428ac5c
alminv <vehabovic.almin@gmail.com>
2016-03-02 21:33:25 +0000

    Sorted URL field.

--- tenderIT/TenderITApp/forms.py
+++ tenderIT/TenderITApp/forms.py
@@ -12,21 +12,13 @@ class UserForm(forms.ModelForm):
 		fields = ('username','password')
 
 class CompanyForm(forms.ModelForm):
+	
+	website = forms.URLField(max_length=200, help_text="Please enter the URL of the page.", initial='http://')
+
 	class Meta:
 		model = Company
 		fields = ('name', 'nationalID', 'street','city','country','postcode', 'email', 'phone', 'website')	
-	
-#class Registration_form(UserCreationForm):
- #   class Meta:
-  #      model = Company
-  #      fields = ('name', 'username', 'email', 'password1', 'password2')
 
- #   def save(self, commit=True):
- #       user = super(Registration_form, self).save(commit = False)
- #       user.email = self.cleaned_data['email']
- #       if commit:
- #           user.save()
- #       return user
 
 # form to add new project
 class Post_project(forms.ModelForm):
