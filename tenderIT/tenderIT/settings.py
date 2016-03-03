cc8f05fa19bfe00bd4f9bad178ca724d7eb81778
alminv <vehabovic.almin@gmail.com>
2016-03-03 21:46:16 +0000

    Created static directory, moved css to static. Fixed login and registration links on index.

--- tenderIT/tenderIT/settings.py
+++ tenderIT/tenderIT/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,3 +84,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+	STATIC_PATH,
+)
