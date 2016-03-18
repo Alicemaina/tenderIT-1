d04ca8917cbe9c06958169cffc52656066529f5c
GRUSTAMLI <qedir_rustemli@hotmail.com>
2016-03-18 17:30:46 +0000

    latest

--- tenderIT/TenderITApp/forms.py
+++ tenderIT/TenderITApp/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
-<<<<<<< HEAD
+
 from .models import (Company, Project)
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, MultiField, Div, Field
-=======
+
 from .models import (Company, Project, ProjectApplication)
->>>>>>> 27ad52eb056c3c1a06cf2735939286219a2f67a2
 
 from django_countries.fields import CountryField
 from django_countries.widgets import CountrySelectWidget
@@ -84,10 +83,6 @@ class Post_project(forms.ModelForm):
 	class Meta:
 		model = Project
 		fields = ('title', 'description', 'budget', 'currency', 'startDate', 'endDate')
-<<<<<<< HEAD
-		
-=======
-
 # Apply for a project		
 class Apply_project(forms.ModelForm):
 	price = forms.IntegerField(min_value = 1, error_messages={'required': 'Please enter offered price.', 'min_value':'Price cannot be less than one.'})
@@ -102,4 +97,3 @@ class Login_form(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput)
 
->>>>>>> 27ad52eb056c3c1a06cf2735939286219a2f67a2
