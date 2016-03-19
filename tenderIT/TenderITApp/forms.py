4ae64c4fe68839a8bb100048fbde89ec28fd9403
GRUSTAMLI <qedir_rustemli@hotmail.com>
2016-03-19 01:36:46 +0000

    Gadir's

--- tenderIT/TenderITApp/forms.py
+++ tenderIT/TenderITApp/forms.py
@@ -4,12 +4,9 @@ from django.contrib.auth.models import User
 from .models import (Company, Project)
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, MultiField, Div, Field
+from crispy_forms.bootstrap import AppendedText
 
 from .models import (Company, Project, ProjectApplication)
-<<<<<<< HEAD
-=======
-
->>>>>>> 7430ee709604560b10937f8ecfaa90cc6f741d13
 
 from django_countries.fields import CountryField
 from django_countries.widgets import CountrySelectWidget
@@ -60,10 +57,10 @@ class CompanyForm(forms.ModelForm):
 # form to add new project_templates
 class Post_project(forms.ModelForm):
 	title = forms.CharField(min_length=8, max_length=128, error_messages={'required': 'Please enter project title.', 'min_length':'Project title must contain at least eight charachters.'}, widget=forms.TextInput(attrs={'placeholder': 'Enter project title...', }))
-	description = forms.CharField(min_length=32, max_length=2048, error_messages={'required': 'Please enter project description.', 'min_length':'Project description must contain at least 32 charachters.'}, widget=forms.Textarea(attrs={'placeholder': 'Enter project description...'}))
+	description = forms.CharField(min_length=32, max_length=2048, error_messages={'required': 'Please enter project description.', 'min_length':'Project description must contain at least 32 charachters.'}, widget=forms.Textarea(attrs={'placeholder': 'Enter project description...','rows': 6, 'cols': 10}))
 	budget = forms.IntegerField(min_value = 1, error_messages={'required': 'Please enter project budget.', 'min_value':'Projest budget cannot be less than one.'})
-	startDate = forms.DateField(initial = datetime.datetime.now().date(),widget=SelectDateWidget)
-	endDate =  forms.DateField(initial = datetime.datetime.now().date(),widget=SelectDateWidget)
+	startDate = forms.DateField(initial = datetime.datetime.now().date())
+	endDate =  forms.DateField(initial = datetime.datetime.now().date())
 	
 	def clean(self):
 		cleaned_data = super(Post_project, self).clean()
@@ -80,20 +77,42 @@ class Post_project(forms.ModelForm):
 			if endDate <= startDate:
 				msg = u'End date must be after start date.'
 				self.add_error('endDate',msg)
-		
-		
 		return cleaned_data
+
+	def __init__(self, *args, **kwargs):
+		super(Post_project, self).__init__(*args, **kwargs)
+		self.helper = FormHelper(self)
+		self.helper.form_class= 'form-horizontal'
+		self.helper.label_class= 'project_label col-lg-3 project_container'
+		self.helper.field_class= 'project_field col-lg-8 project_container'
+		self.helper.layout = Layout(
+			Div(
+				Field('title', wrapper_class="project_jumbo jumbotron row project_container", css_class="project_container" ,),
+				Field('description', wrapper_class="description project_jumbo jumbotron row project_container", css_class="vresize"),
+				Field('budget', wrapper_class="project_jumbo jumbotron row project_container", css_class="project_container"),
+				Field('currency', wrapper_class="project_jumbo jumbotron row project_container", css_class="project_container"),
+				css_class= "project_container col-lg-5 col-lg-offset-1 col-md-6 "
+			),
+
+			Div(
+				Field('startDate', wrapper_class="project_jumbo jumbotron row project_container", css_class="project_container"),
+				Field('endDate', wrapper_class="project_jumbo jumbotron row project_container",css_class="project_container"),
+
+				Submit('Post Project', 'Postproject', css_class="greenbtn btn-block project_container"),
+				css_class= "project_container col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1"
+
+
+			),
+
+		)
+
+
 
 	class Meta:
 		model = Project
 		fields = ('title', 'description', 'budget', 'currency', 'startDate', 'endDate')
-<<<<<<< HEAD
-=======
-
-		
 
 
->>>>>>> 7430ee709604560b10937f8ecfaa90cc6f741d13
 # Apply for a project		
 class Apply_project(forms.ModelForm):
 	price = forms.IntegerField(min_value = 1, error_messages={'required': 'Please enter offered price.', 'min_value':'Price cannot be less than one.'})
@@ -108,7 +127,5 @@ class Login_form(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput)
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 7430ee709604560b10937f8ecfaa90cc6f741d13
+
---
+++ tenderIT/static/js/script.js
@@ -0,0 +1,22 @@
+$(function() {	
+    $( "#id_startDate" ).datepicker({
+    	showAnim: 'slideDown',
+    	numberOfMonths:1,
+    	// dateFormat: 'dd-mm-yy',
+    	minDate: 0,
+    	onclose: function( selectedDate ){
+    		$("#id_endDate").datepicker("option", "minDate", selectedDate);
+    	}
+
+    });
+  
+	
+    $( "#id_endDate" ).datepicker({
+    	showAnim: 'slideDown',
+    	numberOfMonths:1,
+    	// dateFormat: 'dd-mm-yy',
+    	onclose: function( selectedDate ){
+    		$("#id_startDate").datepicker("option", "maxDate", selectedDate);
+    	}
+    });
+  });
