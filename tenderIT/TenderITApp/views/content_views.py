2f748a091e584392e4e94254b598d92fa3f8ff92
alminv <vehabovic.almin@gmail.com>
2016-03-22 10:30:54 +0000

    Sorted space issue with content views, added tests.py.

--- tenderIT/TenderITApp/views/content_views.py
+++ tenderIT/TenderITApp/views/content_views.py
@@ -6,21 +6,21 @@ from django.shortcuts import get_object_or_404
 
 # Home page view
 def index(request):
-    c = {}
-    return render(request, 'index.html', c)
+	c = {}
+	return render(request, 'index.html', c)
 
 # All companies view, handels request for all companies, orders companies by name
 def companies(request):
-    company_list = Company.objects.order_by('name')	
-    context_dict = {'companies' : company_list}
-    return render(request, 'companies.html', context_dict)
+	company_list = Company.objects.order_by('name')	
+	context_dict = {'companies' : company_list}
+	return render(request, 'companies.html', context_dict)
 
 # Single company view, handles request for a company based on company id (primary key)
 def company(request, company_id):
 	context_dict = {}	
 	try:
 		company = Company.objects.get(pk=company_id) 		# Get company by company id
-   		context_dict['company'] = company		
+		context_dict['company'] = company		
 		projects = Project.objects.filter(company=company)  # Get company projects
 		context_dict['projects'] = projects	
 	# Company does not exist, do nothing
@@ -40,28 +40,28 @@ def project(request, project_pk):
 	context_dict = {}
 	own_project = False		# Variable to show does the project belong to authenticated user
 	try:	
-	 project = Project.objects.get(pk = project_pk)  # Get project by primary key
+		project = Project.objects.get(pk = project_pk)  # Get project by primary key
 	 
 	 # If user is authenticated check is he the owner of the project and check did he already applied for this project
-	 if request.user.is_authenticated(): 
-		company = Company.objects.get(user=request.user)
-		if company == project.company:
-			own_project = True
-		try:
-			apply_exist = ProjectApplication.objects.get(applicant=company, project=project)
-		except ProjectApplication.DoesNotExist:
-			apply_exist = None
+		if request.user.is_authenticated(): 
+			company = Company.objects.get(user=request.user)
+			if company == project.company:
+				own_project = True
+			try:
+				apply_exist = ProjectApplication.objects.get(applicant=company, project=project)
+			except ProjectApplication.DoesNotExist:
+				apply_exist = None
 		# Get project application for this project
 		applications = ProjectApplication.objects.filter(project=project)
 
-	 context_dict['applications'] = applications
-	 context_dict['project'] = project
-	 context_dict['own_project'] = own_project
-	 context_dict['apply_exist'] = apply_exist
+		context_dict['applications'] = applications
+		context_dict['project'] = project
+		context_dict['own_project'] = own_project
+		context_dict['apply_exist'] = apply_exist
 	
 	# Project does not exist, do nothing
 	except Project.DoesNotExist:
-	 pass
+		pass
 	
 	return render(request, 'project_templates/project_view.html', context_dict)
 
@@ -71,18 +71,18 @@ def post_project(request):
 	project_added = False  # Value to tell the template is the project added.
 
 	# If request is POST check form validity and create new project object
-   	if request.method == 'POST':
+	if request.method == 'POST':
 		project_form = Post_project(request.POST, request.FILES)
-        	if project_form.is_valid():
-				project=project_form.save(commit=False)
-				project.company = Company.objects.get(user=request.user)
-				project.avatar = request.FILES['avatar']
-				project.document = request.FILES['document']
-				project.save()
-				project_added=True	
+		if project_form.is_valid():
+			project=project_form.save(commit=False)
+			project.company = Company.objects.get(user=request.user)
+			project.avatar = request.FILES['avatar']
+			project.document = request.FILES['document']
+			project.save()
+			project_added=True	
 	# Request not POST, show the form			               
 	else:
-         project_form = Post_project()
+		project_form = Post_project()
 
 	context = {
         'form' : project_form,
@@ -126,21 +126,21 @@ def apply_project(request, project_id):
 	application_added = False # Value to tell the template whether the application was sucessfull
 	project	= Project.objects.get(id=project_id) # Get project by project id
 	# If request is POST check form validity and create new ProjectApplication
-   	if request.method == 'POST':
+	if request.method == 'POST':
 		apply_form = Apply_project(request.POST)
-        	if apply_form.is_valid():
-				application = apply_form.save(commit=False)
-				application.project = Project.objects.get(id=project_id)
-				application.applicant = Company.objects.get(user=request.user)
-				application.save()				
-				application_added=True	
-				project = application.project	
+		if apply_form.is_valid():
+			application = apply_form.save(commit=False)
+			application.project = Project.objects.get(id=project_id)
+			application.applicant = Company.objects.get(user=request.user)
+			application.save()				
+			application_added=True	
+			project = application.project	
 	               
-	    	else:
-				print apply_form.errors
+		else:
+			print apply_form.errors
 	# Request not POST, show the form
 	else:
-         	apply_form = Apply_project()
+		apply_form = Apply_project()
 
 	context = {
         'form' : apply_form,
@@ -168,6 +168,6 @@ def application_edit(request, application_id):
  # Left for better days.
 @login_required
 def rate_project(request, project_id):   
-    return 
+	return 
 
 
