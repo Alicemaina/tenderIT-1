242afec84bcec00b0a331d98d3f560582be32b2a
GRUSTAMLI <qedir_rustemli@hotmail.com>
2016-03-23 11:53:07 +0000

    Finished

--- tenderIT/TenderITApp/templatetags/app_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-from ..models import Project
-
-
-# # @register.filter(name="is_own_project"):
-# def check_project_owner(project_id):
-#
-#
